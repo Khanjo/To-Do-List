e2ce03d5892a24e02c2d0c21202ad0dd776a5e12
Joshua Khan <Khanjo@gmail.com>
2023-07-31 11:19:18 -0700

    complete CRUD for tags and CRD for ItemTag

--- ToDoList/Controllers/TagsController.cs
+++ ToDoList/Controllers/TagsController.cs
@@ -18,6 +18,7 @@ namespace ToDoList.Controllers
 
         public ActionResult Index()
         {
+            ViewBag.PageTitle = "View All Tags";
             return View(_db.Tags.ToList());
         }
 
@@ -25,6 +26,79 @@ namespace ToDoList.Controllers
         {
             Tag thisTag = _db.Tags.Include(tag => tag.JoinEntities).ThenInclude(join => join.Item).FirstOrDefault(tag => tag.TagId == id);
             return View(thisTag);
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.PageTitle = "Create New tag";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Tag tag)
+        {
+            _db.Tags.Add(tag);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult AddItem(int id)
+        {
+            Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            ViewBag.ItemId = new SelectList(_db.Items, "ItemId", "Description");
+            return View(thisTag);
+        }
+
+        [HttpPost]
+        public ActionResult AddItem(Tag tag, int itemId)
+        {
+#nullable enable
+            ItemTag? joinEntity = _db.ItemTags.FirstOrDefault(join => (join.ItemId == itemId && join.TagId == tag.TagId));
+#nullable disable
+            if (joinEntity == null && itemId != 0)
+            {
+                _db.ItemTags.Add(new ItemTag() { ItemId = itemId, TagId = tag.TagId });
+                _db.SaveChanges();
+            }
+            return RedirectToAction("Details", new { id = tag.TagId });
+        }
+
+        public ActionResult Edit(int id)
+        {
+            Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            return View(thisTag);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Tag tag)
+        {
+            _db.Tags.Update(tag);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            return View(thisTag);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tag thisTag = _db.Tags.FirstOrDefault(tags => tags.TagId == id);
+            _db.Tags.Remove(thisTag);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult DeleteJoin(int joinId)
+        {
+            ItemTag joinEntry = _db.ItemTags.FirstOrDefault(entry => entry.ItemTagId == joinId);
+            _db.ItemTags.Remove(joinEntry);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }

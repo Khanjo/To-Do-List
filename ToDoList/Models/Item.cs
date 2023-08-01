faab5be30655f20fbbbcd2edf42b8284c80f0bd0
Joshua Khan <Khanjo@gmail.com>
2023-08-01 09:32:54 -0700

    add require item description

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
     public class Item
     {
+        [Required(ErrorMessage = "The item's description cannot be empty")]
         public string Description { get; set; }
         public int ItemId { get; set; }
+        [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category, please add one first")]
         public int CategoryId { get; set; }
         public bool Completed { get; set; } = false;
         public Category Category { get; set; }

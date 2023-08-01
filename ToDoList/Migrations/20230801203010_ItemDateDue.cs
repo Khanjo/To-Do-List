5b314b741c0cbbff6f5b884c794aeaa8f062d3c5
Joshua Khan <Khanjo@gmail.com>
2023-08-01 13:44:47 -0700

    test adding DateTime to items and then removed

---
+++ ToDoList/Migrations/20230801203010_ItemDateDue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class ItemDateDue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateDue",
+                table: "Items",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDue",
+                table: "Items");
+        }
+    }
+}
---
+++ ToDoList/Migrations/20230801203725_RemoveDateDue.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoList.Migrations
+{
+    public partial class RemoveDateDue : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateDue",
+                table: "Items");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateDue",
+                table: "Items",
+                type: "date",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+    }
+}
